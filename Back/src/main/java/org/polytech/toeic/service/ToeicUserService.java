48ff9666d247f865e4efa698f2ebf2906ca64043
Galaxycorn <44713376+Galaxycorn@users.noreply.github.com>
2023-01-23 15:40:14 +0100

    Add working login page

--- Back/src/main/java/org/polytech/toeic/service/ToeicUserService.java
+++ Back/src/main/java/org/polytech/toeic/service/ToeicUserService.java
@@ -60,7 +60,7 @@ public class ToeicUserService implements UserDetailsService {
 
     @PostConstruct
     public void Test() {
-        if (toeicUserRepository.findByName("admin")==null)
+        if (toeicUserRepository.findByName("admin") != null)
         {
             log.info("Création de l'utilisateur par defaut");
             ToeicUser user = new ToeicUser();
--- Front/src/routes/login/+page.svelte
+++ Front/src/routes/login/+page.svelte
@@ -1,31 +1,33 @@
 <script>
+  import {Buffer} from 'buffer';
+  import { writable } from "svelte/store";
     /** @type {import('./$types').PageData} */
-    let users = [];
+let users = [];
 let name = ""
 let password = ""
 let admin = false
-
-/**
-   * @type {string | null}
-   */
-let result = null
-/* GET EXAMPLE
-onMount(async () => {
-  const res = await fetch(`http://localhost:8080/api/public/user`);
-   users = await res.json();
-});*/
+const store = writable();
 
 async function login () {
+  //On utilise un token Basic auth pour se connecter 
+  var hash = name + ":" + password;
+  var token = Buffer.from(hash).toString('base64');
+  var auto = "Basic " + token;
+  console.log(auto);
   const res = await fetch('http://localhost:8080/api/public/login', {
     method: 'GET',
-    headers: {'Content-Type' : 'application/json'}
+    headers: {
+      'Content-Type' : 'application/json',
+      'Authorization' : auto
+    }
   })
-
-  const json = await res.json()
-  result = JSON.stringify(json)
+  .then((response) => response.json())
+  .then((data) => {
+  console.log(data),
+  store.subscribe(datas => localStorage.setItem('store', JSON.stringify(data)))}
+  );
 }
 
-console.log(admin)
 
 </script>
 

08a8056dc95d5b00e41fd8b62dcffb8a7998200b
Galaxycorn <juliann.gaillard@hotmail.fr>
2023-02-01 02:48:11 +0100

    Add link between

--- Front/src/routes/forum/[slug]/+page.svelte
+++ Front/src/routes/forum/[slug]/+page.svelte
@@ -1,1 +1,11 @@
-<h1>J'AIME PAS MACRON</h1> 
+<script lang="ts">
+    import { page } from '$app/stores'
+
+    const url = $page.url.toJSON();
+    let id; //To not raise an error with item[id]
+    id = url.split("forum/")
+    export let data; //We export the fetch result here with data from the page.ts load function
+</script>
+
+<h1>{data.item[id[1]-1].content}</h1>
+
